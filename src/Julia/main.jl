7a65b35b360969734b7f3b598548f4a2e157bc3c
nwelch <nwelch@uw.edu>
2017-05-02 23:36:19 -0700

    Jobrunner commit

---
+++ src/Julia/main.jl
@@ -0,0 +1,12 @@
+#!/usr/bin/env julia
+
+mu = 0.003;
+sigma = 5.0;
+theta = 0.105;
+
+include("/Users/nwelch/prelim/src/Julia/metropolis_sampler.jl");
+
+df = readtable("/Users/nwelch/prelim/data/plantDataTest.csv");
+dst = readtable("/Users/nwelch/prelim/data/plantDistanceTest.csv");
+
+run_mcmc_sampler(mu, theta, sigma, df, dst, trials=5000)
