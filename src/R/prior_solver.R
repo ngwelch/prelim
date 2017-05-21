bb6530387fa22187a3d52b133f9cbe51e71f5d10
nwelch <nwelch@uw.edu>
2017-05-21 14:47:23 -0700

    First prior solver and correct simulation methods

--- src/R/prior_solver.R
+++ src/R/prior_solver.R
@@ -1,13 +1,14 @@
+library(MASS)
+library(xtable)
+
 # mu prior
-get_a = function(B, alpha=0.05){
+get_mu_a = function(B, alpha=0.05){
   a = log(alpha/2)/log(B/(1+B))
   return(a)
 }
 
 get_mu_dtail = function(B, xmax=630, alpha=0.05){
-  a = get_a(B, alpha)
-  p = 1/(1+B)
-  oneMinusP = 1-p
+  a = get_mu_a(B, alpha)
   
   tailProb = 0
   for(x in 0:(xmax-1)){
@@ -19,14 +20,86 @@ get_mu_dtail = function(B, xmax=630, alpha=0.05){
   return(-out)
 }
 
-b = uniroot(get_mu_dtail, c(0.0001, 10))
+b = uniroot(get_mu_dtail, c(0.0001, 10), tol=.Machine$double.eps^0.5)
 
+q=c(0.025, 0.975)
+replicatedQuantile = qgamma(q, shape=get_mu_a(b$root), rate=b$root)
+replicatedMean = get_mu_a(b$root)/b$root
+replicatedVar = get_mu_a(b$root)/(b$root**2)
 
-# theta prior
-get_theta_dtail = function(B, xmax=1920, alpha=0.05){
-  a = get_a(B, alpha)
-  out = a*log(B) - a*log(B+xmax) - log(0.975)
-  return(-out)
+brownQuantile = qgamma(q, shape=0.7, rate=0.004)
+brownMean = 0.7/0.004
+brownVar = 0.7/(0.004**2)
+
+muPriorTable = data.frame(rbind(c(brownQuantile, brownMean, brownVar), 
+                          c(replicatedQuantile, replicatedMean, replicatedVar)))
+colnames(muPriorTable) = c("2.5%", "97.5%", "mean", "variance")
+rownames(muPriorTable) = c("Brown et. al.", "Replicated")
+tmp = xtable(muPriorTable, caption = "")
+print(tmp)
+
+# sigma prior
+geta = function(a,b=10){
+  3.9*(gamma(a) - pgamma(b*0.1,a, lower=F)*gamma(a))-gamma(a)+pgamma(b*50,a, lower=F)*gamma(a)
 }
 
-uniroot(get_theta_dtail, c(0.0001, 100))
+a=seq(0.1, 1,by=0.05)
+b=seq(5,200,by=1)
+N=10000
+eps=1e-3
+coverage = matrix(0, nrow=length(a)*length(b), ncol=4)
+colnames(coverage) = c("a", "b", "cover", "abCoversIt")
+it=1
+for(i in a){
+  for(j in b){
+    smpl = rgamma(N, shape=i, scale=j)
+    F500 = sum(smpl<500)/N
+    F0.1 = sum(smpl<0.1)/N
+    cover_ij = F500 - F0.1
+    abCoversIt = F500<0.975 & F0.1<0.025
+    #abCoversIt = ifelse(abs(cover_ij-0.95)<=eps, TRUE, FALSE)
+    coverage[it,] = c(i, j, cover_ij, abCoversIt)
+    it = it+1
+  }
+}
+coverage = as.data.frame(coverage)
+coverage = coverage[coverage$abCoversIt==TRUE,]
+weakestSigmaPrior = coverage[which.max(coverage$a*(coverage$b**2)),]
+
+# theta prior
+source('~/prelim/src/R/simulation.R')
+tmp = read.csv(file="/Users/nwelch/prelim/data/plantData.csv")[,c('x', 'y')]
+tmp = sort(unique(tmp$x + 1.0i*tmp$y))
+xi = tmp[Re(tmp)<=4.5 & Im(tmp)<=1]
+dxixj = Mod(outer(xi, xi, FUN="-"))
+
+it=1
+theta = seq(0.05, 0.2, by=0.01)
+#52.6225#seq(0.5, 2, by=0.05)
+sigma = sort(rgamma(10, shape=weakestSigmaPrior$a, scale=weakestSigmaPrior$b))
+sampleSize=5
+coverage = matrix(0, nrow=length(theta)*length(sigma), ncol=3)
+colnames(coverage) = c("theta", "sigma", "cover")
+#out = array(dim=c(sampleSize,9,length(theta)))
+
+for(th in theta){
+  for(s in sigma){
+    betweenDay1And448=0
+    for(i in 1:sampleSize){
+      for(j in 1:9){
+        seed = rep(TRUE, nrow(dxixj))
+        seed[j] = FALSE
+        infDay = scalarEpiSim(sigma=s, mu=1e-5, theta=th,
+                              startTime=0, endTime=Inf, susceptableAtStart=seed, 
+                              dxixj=dxixj, N=9, simCount=1, returnSummary=FALSE)*7
+        nextInfDay = min(infDay[infDay>0], na.rm=TRUE)
+        betweenDay1And448 = betweenDay1And448+(nextInfDay>=1 & nextInfDay<=448)
+      }
+    }
+    coverage[it,] = c(th, s, betweenDay1And448/(9*sampleSize))
+    it = it+1
+  }
+}
+coverage = as.data.frame(coverage)
+candidateMeans = coverage[round(coverage$cover,2)==0.95,]
+colMeans(candidateMeans)
---
+++ src/R/simulation.R
@@ -0,0 +1,220 @@
+dst = read.csv(file="/Users/nwelch/prelim/data/plantDistance.csv")
+
+
+##############################################################################
+########################## Epidemic Simulation ###############################
+##############################################################################
+### Simulated Epidemic Function
+scalarEpiSim = function(sigma=1.1, mu=0.0028, theta=0.11,
+                  startTime=0, endTime=30, susceptableAtStart=rep(TRUE,nrow(dst)), 
+                  dxixj=dst, N=nrow(dst), simCount=1, returnSummary=TRUE){
+  
+  dstSq = dxixj**2
+  twoSigmaSq = 2*(sigma**2)
+  thetafxixj = (theta/(twoSigmaSq*pi))*exp(-dstSq/twoSigmaSq)
+  
+  weeks = c(0, seq(0.5, min(endTime-0.5, 69.5)))
+  out = matrix(weeks, nrow=length(weeks), ncol=(simCount+1))
+
+  # Force only one sim if the goal is to simulate infection times
+  if(!returnSummary){simCount=1}
+  for(s in 1:simCount){
+    
+    origInfTimes = Inf
+    if(sum(susceptableAtStart==FALSE)>0){
+      ratesAtInfectedPlants = thetafxixj[,!susceptableAtStart, drop=FALSE]
+      sampleNewInfTimes = apply(ratesAtInfectedPlants, 2, function(r) rexp(N, r))
+      origInfTimes = startTime + sampleNewInfTimes
+    }
+    
+    infTime = pmin(startTime + rexp(N, mu), origInfTimes, endTime)
+    susceptibles = which((infTime < endTime) & susceptableAtStart)
+    
+    while(length(susceptibles)){
+      
+      currentTime = min(infTime[susceptibles])
+      currentPlant = susceptibles[which.min(infTime[susceptibles])]
+      currentPlantRates = thetafxixj[,currentPlant]
+      
+      nextInfectionTime = rexp(N, currentPlantRates)
+      nextInfectionTime[is.na(nextInfectionTime)] = Inf
+      infTime = pmin(infTime, currentTime + nextInfectionTime)
+      susceptibles = which((infTime < endTime) & (infTime > currentTime) & 
+                             susceptableAtStart)
+    }
+    infTime = ifelse(infTime>=endTime, Inf, infTime)
+    infTime[!susceptableAtStart] = startTime
+    
+    # create an index and count up number of infections per index interval
+    results=c()
+    for(w in weeks){
+      results = c(results, sum(abs(infTime-w)<0.5))
+    }
+   out[,s+1] = results 
+  }
+  
+  if(returnSummary){
+    colnames(out) = c('week', paste0('sim', 1:simCount))
+    return(data.frame(out))
+  } else {
+    return(infTime)
+  }
+}
+
+oneSim = scalarEpiSim()
+timeSim = scalarEpiSim(returnSummary = FALSE)
+
+### Sampler that runs over a vector of parameter values
+vectorEpiSim = function(muSigmaThetaVector, startTime=0, endTime=30, 
+                        susceptableAtStart=rep(TRUE,nrow(dst)), dxixj=dst, 
+                        N=nrow(dst), simCount=1, returnSummary=TRUE){
+  
+  if(returnSummary){
+    result = NULL
+    for(s in 1:nrow(muSigmaThetaVector)){
+      mu = muSigmaThetaVector[s,1]
+      sigma = muSigmaThetaVector[s,2]
+      theta = muSigmaThetaVector[s,3]
+      sim = scalarEpiSim(sigma, mu, theta, startTime, endTime, susceptableAtStart,
+                         dxixj, N, simCount, returnSummary=TRUE)
+      
+      if(s>1){
+        result = cbind(result, sim[,-1, drop=FALSE])
+      } else {
+        result = sim
+      }
+    }
+    tmp = apply(result[,-1], 1, function(w) c(mean(w), quantile(w, c(0.025, 0.975))))
+    sampleCI = t(tmp)
+    colnames(sampleCI)[1] = "mean"
+    
+    out = cbind(result[,1,drop=FALSE], sampleCI, result[,-1,drop=FALSE])
+    
+  } else {
+    out = NULL
+    for(s in 1:nrow(muSigmaThetaVector)){
+      mu = muSigmaThetaVector[s,1]
+      sigma = muSigmaThetaVector[s,2]
+      theta = muSigmaThetaVector[s,3]
+      sim = scalarEpiSim(sigma, mu, theta, startTime, endTime, susceptableAtStart,
+                         dxixj, N, simCount=1, returnSummary=FALSE)
+      out=cbind(out, sim)
+    }
+    colnames(out) = paste0("sim", 1:nrow(muSigmaThetaVector))
+  }
+  return(data.frame(out))
+}
+
+u = c(0.0028, 0.0025, 0.003)
+s = c(1.1, 1.15, 1.08)
+t = c(0.11, 0.08, 0.15)
+vecSim = vectorEpiSim(muSigmaThetaVector=cbind(u,s,t), simCount=10)
+
+matplot(x=vecSim$week, y=vecSim[,5:ncol(vecSim)], type='l', lty=3, col="#50505030",
+        ylab="infections per week", xlab="week")
+matlines(x=vecSim$week, y=vecSim[,2:4], col='black', lty=c(1,2,2), lwd=3)
+
+##############################################################################
+########################## Posterior Sampler #################################
+##############################################################################
+postEpiSim = function(tau){
+  
+  # create an index and count up number of infections per index interval
+  weeks = c(0, seq(0.5, 29.5))
+  post = NULL
+  for(w in weeks){
+    post = cbind(post, apply(tau, 1, function(t) sum(abs(t-w)<0.5)))
+  }
+  rownames(post) = paste0('sample', 1:nrow(tau))
+  colnames(post) = paste0('week', weeks)
+  
+  # find the CI for each of sample
+  postCI = apply(post, 2, function(w) c(mean(w), quantile(w, c(0.025, 0.975))))
+  rownames(postCI)[1] = "mean"
+  
+  # return both results in the same matrix
+  rbind(weeks, postCI, post)
+}
+
+sampleTau = read.csv(file="~/prelim/data/mcmc_chain_julia.csv")[,1:34]
+pstSim = postEpiSim(sampleTau)
+
+matplot(x=pstSim['weeks',], y=t(pstSim[5:200,]), type='l', lty=3, col="#50505030",
+        xlab="infections per week", ylab="week")
+matlines(x=pstSim['weeks',], y=t(pstSim[2:4,]), col='black', lty=c(1,2,2), lwd=3)
+
+##############################################################################
+############################ Density Plots ###################################
+##############################################################################
+
+### First time period
+breakPoints = seq(10,14,len=10)
+tmp = apply(sampleTau[,1:7], 2, hist, breaks=breakPoints, plot=F)
+postSampleDensity = NULL
+for(psd in 1:length(tmp))
+  postSampleDensity = cbind(postSampleDensity, tmp[[psd]]$density)
+
+matplot(breakPoints[-1]-diff(breakPoints)/2, postSampleDensity, 
+        type="l", lty=rep(1:3, c(3,3,3)),
+        col=rep(grey(c(0, 0.4, 0.6, 0.8)),3),
+        xlab="week", ylab="density")
+
+### Last time period
+breakPoints = seq(23,30,len=10)
+tmp = apply(sampleTau[,25:34], 2, hist, breaks=breakPoints, plot=F)
+postSampleDensity = NULL
+for(psd in 1:length(tmp))
+  postSampleDensity = cbind(postSampleDensity, tmp[[psd]]$density)
+
+postSampleMean = apply(postSampleDensity, 2, mean)
+postSampleMean = matrix(postSampleMean, nrow(postSampleDensity), 
+                        ncol(postSampleDensity))
+centering = postSampleDensity - postSampleMean
+centeringL = apply(centering[1:3,], 2, sum)
+centeringH = apply(centering[7:9,], 2, sum)
+
+plotLines=c(order(centeringL, decreasing=T)[c(1,10)],
+            order(centeringH, decreasing=T)[c(1,10)])
+
+matplot(breakPoints[-1]-diff(breakPoints)/2, postSampleDensity, 
+        type="l",
+        col="#50505030",
+        xlab="week", ylab="density")
+matlines(breakPoints[-1]-diff(breakPoints)/2, 
+         postSampleDensity[,toplot], 
+         col="white", lty=1, lwd=4)
+matlines(breakPoints[-1]-diff(breakPoints)/2, 
+         postSampleDensity[,toplot], 
+         col="black",lty=c(1,1,2,2))
+
+##############################################################################
+############################# Prediction #####################################
+##############################################################################
+
+t6 = read.csv(file="/Users/nwelch/prelim/data/plantData.csv")$t6
+susceptable = ifelse(t6==0, TRUE, FALSE)
+onePredSim = scalarEpiSim(startTime=30, endTime=Inf, 
+                          susceptableAtStart=susceptable,
+                          returnSummary=FALSE)
+testMatrix = cbind(u,s,t)
+for(i in 1:4){ testMatrix = rbind(testMatrix, testMatrix)}
+manyPredSim = vectorEpiSim(muSigmaThetaVector=testMatrix,
+                           startTime=30, endTime=Inf, 
+                           susceptableAtStart=susceptable,
+                           returnSummary=FALSE)
+
+cumTimes = c(35, 40, 50, 60)
+colBreaks = c(-0.1, 0.2, 0.8, 0.9999, 1.01)
+colLabel = c('zeroTo19', 'twentyTo79', 'eightyTo99', 'knownInfection')
+probInfected = probInfLabel = NULL
+for(ct in cumTimes){
+  probabilities = apply(manyPredSim, 1, function(t) mean(t<ct))
+  probInfected = cbind(probInfected, probabilities)
+  probLabels = as.character(cut(probabilities, breaks=colBreaks, labels=colLabel))
+  probInfLabel = cbind(probInfLabel, probLabels)
+}
+colnames(probInfected) = colnames(probInfLabel) = paste0("week", cumTimes)
+write.csv(probInfected, file="/Users/nwelch/prelim/data/fig5Probabilities.csv", 
+          row.names=FALSE)
+write.csv(probInfLabel, file="/Users/nwelch/prelim/data/fig5ProbLabels.csv", 
+          row.names=FALSE)
