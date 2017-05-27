e377289b8d038e9dc5835fc4fad83a5a306b13fd
nwelch <nwelch@uw.edu>
2017-05-26 19:44:00 -0700

    MCMC run settings that produce a 5k obs independent sample from a run using 65k sweaps through the posterior with 164 plants

--- src/Julia/main.jl
+++ src/Julia/main.jl
@@ -10,18 +10,17 @@ mu = 4.85996;
 sigma = 1.0;
 theta = 0.1;
 
-run_mcmc_sampler(mu, theta, sigma, df, dxixj, trials=10000,
+run_mcmc_sampler(mu, theta, sigma, df, dxixj, trials=65000,
                      mu_shape=1.307877, mu_rate=0.06334962,
                      theta_shape=0.8, theta_rate=0.1,
                      sigma_shape=0.5, sigma_scale=100,
-                     v_mu=0.005, v_sigma=0.5, v_theta=0.05)
+                     v_mu=0.005, v_sigma=0.55, v_theta=0.05)
 
-#previous values, theta acceptance is too high
 #v_mu: 0.005    |-      |-      |-  |new th
 #v_sg: 0.005    |0.01   |0.1    |1  |new th
 #v_th: 0.05     |-      |-      |-  |new th
 
-#previous values, sigma acceptance is too low
-#v_mu: 0.005    |
-#v_sg: 0.5      |0.005
-#v_th: 0.05     |
+#previous values, sigma autocorrelation too high
+#v_mu: 0.005    |-      |-      |-
+#v_sg: 0.5      |0.005  |0.5    |0.75
+#v_th: 0.05     |-      |-      |-
